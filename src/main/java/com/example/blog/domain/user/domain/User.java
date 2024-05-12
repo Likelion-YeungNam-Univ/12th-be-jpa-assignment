c0499226f233c2d6469bb58fad15eabd572f64b9
Jsplix <jkjin112@gmail.com>
2024-05-13 00:48:17 +0900

    User Entity 구현

--- src/main/java/com/example/blog/domain/user/domain/User.java
+++ src/main/java/com/example/blog/domain/user/domain/User.java
@@ -6,26 +6,42 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
+
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
-public class User{
+public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String username;
+
+    @Column(name = "email", length = 50, nullable = false)
+    private String email;
+
+    @Column(name = "password", length = 25, nullable = false)
     private String password;
+
+    @Column(name = "nickname", length = 25, nullable = false)
+    private String nickname;
+
+    @Column(name = "birthdate", nullable = false)
+    private LocalDate birthdate;
 
     // 빌더 패턴
     @Builder
-    public User(String username, String password) {
-        this.username = username;
+    public User(String email, String password, String nickname, LocalDate birthdate) {
+        this.email = email;
         this.password = password;
+        this.nickname = nickname;
+        this.birthdate = birthdate;
     }
 
     //편의 메서드
-    public void update(String username, String password) {
-        this.username=username;
+    public void updatePassword(String password) {
         this.password = password;
+    }
+    public void updateNickname(String nickname) {
+        this.nickname = nickname;
     }
 }
