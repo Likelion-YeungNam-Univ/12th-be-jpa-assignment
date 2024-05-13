6659df8988dcba9b759e70983bb73f36a0132492
Jsplix <jkjin112@gmail.com>
2024-05-14 08:41:21 +0900

    Board Entity 생성

---
+++ src/main/java/com/example/blog/domain/board/domain/Board.java
@@ -0,0 +1,59 @@
+package com.example.blog.domain.board.domain;
+
+import com.example.blog.util.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Board extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "title", length = 30, nullable = false)
+    private String title;
+
+    @Column(name = "content", length = 1024)
+    @ColumnDefault("")
+    private String content;
+
+    @Column(name = "createdAt", nullable = false, updatable = false)
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @Column(name = "updatedAt", nullable = false)
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+    @Column(name = "view")
+    @ColumnDefault("0")
+    private int view;
+
+    @Builder
+    public Board (String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+    public void updateTitle(String title) {
+        this.title = title;
+    }
+
+    public void updateContent(String content) {
+        this.content = content;
+    }
+
+    public void increaseView() {
+        this.view++;
+    }
+}
