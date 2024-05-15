7cb88905bf7fac5fe5f06cb796928c5ffd50c42d
DaeYoung Park <pdy000726@naver.com>
2024-05-16 02:19:30 +0900

    feat - User 도메인 생성

---
+++ src/main/java/com/example/blog/domain/post/domain/Post.java
@@ -0,0 +1,2 @@
+package com.example.blog.domain.post.domain;public class Post {
+}
--- src/main/java/com/example/blog/domain/user/domain/User.java
+++ src/main/java/com/example/blog/domain/user/domain/User.java
@@ -1,6 +1,12 @@
 package com.example.blog.domain.user.domain;
 
-import jakarta.persistence.*;
+import com.example.blog.domain.BaseTimeEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -9,23 +15,47 @@ import lombok.NoArgsConstructor;
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
-public class User{
+public class User extends BaseTimeEntity {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotNull
+    @Column(length = 20)
     private String username;
+
+    @NotNull
+    @Column(length = 20)
     private String password;
+
+    @NotNull
+    private String email;
+
+    @NotNull
+    @Column(length = 10)
+    private String nickname;
+
 
     // 빌더 패턴
     @Builder
-    public User(String username, String password) {
+    public User(String username, String password, String email, String nickname) {
         this.username = username;
+        this.password = password;
+        this.email = email;
+        this.nickname = nickname;
+    }
+
+    // 비밀번호 해쉬
+    public void hashPass(String password) {
         this.password = password;
     }
 
-    //편의 메서드
-    public void update(String username, String password) {
-        this.username=username;
+    // 정보 업데이트
+    public void updateInfo(String password, String email, String nickname) {
         this.password = password;
+        this.email = email;
+        this.nickname = nickname;
     }
+
 }
