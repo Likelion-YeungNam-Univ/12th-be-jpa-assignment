ae9b331d46596ae701a2a2bd5985b9fa71534616
DaeYoung Park <pdy000726@naver.com>
2024-05-16 02:53:59 +0900

    feat - CommentLike 도메인 생성

--- src/main/java/com/example/blog/domain/commentLike/domain/CommentLike.java
+++ src/main/java/com/example/blog/domain/commentLike/domain/CommentLike.java
@@ -1,2 +1,36 @@
-package com.example.blog.domain.commentLike.domain;public class CommentLike {
+package com.example.blog.domain.commentLike.domain;
+
+import com.example.blog.domain.BaseTimeEntity;
+import com.example.blog.domain.comment.domain.Comment;
+import com.example.blog.domain.post.domain.Post;
+import com.example.blog.domain.user.domain.User;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class CommentLike extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private User user;
+
+    private Comment comment;
+
+    @Builder
+    public CommentLike(User user, Comment comment) {
+        this.user = user;
+        this.comment = comment;
+    }
+
+
 }
