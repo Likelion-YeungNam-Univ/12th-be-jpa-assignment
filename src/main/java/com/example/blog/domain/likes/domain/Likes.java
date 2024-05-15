e47d31259edc0ff66940d240bf58ad4b7a320af3
DaeYoung Park <pdy000726@naver.com>
2024-05-16 02:32:40 +0900

    feat - Likes 도메인 생성

--- src/main/java/com/example/blog/domain/likes/domain/Likes.java
+++ src/main/java/com/example/blog/domain/likes/domain/Likes.java
@@ -1,2 +1,33 @@
-package com.example.blog.domain.likes.domain;public class Likes {
+package com.example.blog.domain.likes.domain;
+
+import com.example.blog.domain.BaseTimeEntity;
+import com.example.blog.domain.user.domain.User;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class Likes extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private User follower;  // 추후에 관계매핑
+
+    private User following;  // 추후에 관계매핑
+
+    public void setFollower(User follower) {
+        this.follower = follower;
+    }
+
+    public void setFollowing(User following) {
+        this.following = following;
+    }
 }
