96e0848134cc981d444d69c3481e94d9d61a337f
DaeYoung Park <pdy000726@naver.com>
2024-05-16 02:08:59 +0900

    feat - 생성, 수정 시간 필드생성하는 BaseTimeEntity 생성

--- src/main/java/com/example/blog/domain/BaseTimeEntity.java
+++ src/main/java/com/example/blog/domain/BaseTimeEntity.java
@@ -1,2 +1,45 @@
-package com.example.blog.domain;public class BaseTimeEntity {
+package com.example.blog.domain;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseTimeEntity {
+
+    @NotNull
+    @Column(name = "created_time")
+    @CreatedDate
+    private String created_time;
+
+    @NotNull
+    @Column(name = "modified_time")
+    @LastModifiedDate
+    private String modified_time;
+
+    /* 해당 엔티티를 저장하기 이전에 실행 */
+    @PrePersist
+    public void onPrePersist(){
+        this.created_time = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy.MM.dd HH:mm:ss"));
+        this.modified_time = this.created_time;
+    }
+
+    /* 해당 엔티티를 업데이트 하기 이전에 실행*/
+    @PreUpdate
+    public void onPreUpdate(){
+        this.modified_time = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy.MM.dd HH:mm:ss"));
+    }
 }
