031367a831a120ba8702152f13d154d94d352aa7
wlstj <22020306jinseoyeon@gmail.com>
2024-05-17 21:42:48 +0900

    Reply Entity 추가

---
+++ src/main/java/com/example/blog/reply/domain/Reply.java
@@ -0,0 +1,41 @@
+package com.example.blog.reply.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class Reply {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "replyid")
+    private Long replyId;
+
+    //fk
+    @Column(name = "commentid", nullable = false)
+    private Long commentId;
+
+    @Column(name = "content", nullable = false)
+    private String content;
+
+    @Column(name = "reply_date", nullable = false)
+    private LocalDateTime replyDate;
+
+    @Builder
+    public Reply(Long commentId, String content) {
+        this.commentId = commentId;
+        this.content = content;
+        this.replyDate = LocalDateTime.now();
+    }
+
+    //수정
+    public void updateContent(String content) {
+        this.content = content;
+    }
+}
