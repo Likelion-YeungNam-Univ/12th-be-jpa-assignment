3427f4fe112fa067c952786258f4783ba4cbeb14
icegosimperson <hooho931@gmail.com>
2024-05-17 15:56:54 +0900

    add user private

--- src/main/java/com/example/blog/domain/user/domain/User.java
+++ src/main/java/com/example/blog/domain/user/domain/User.java
@@ -15,17 +15,34 @@ public class User{
     private Long id;
     private String username;
     private String password;
+    // 추가
+    private String nickname;
+    private String gender;
+    private String phone;
+    private String profileImg;
+    private String introduce;
+
 
     // 빌더 패턴
     @Builder
-    public User(String username, String password) {
+    public User(String username, String password, String nickname, String gender, String phone, String profileImg, String introduce) {
         this.username = username;
         this.password = password;
+        this.nickname = nickname;
+        this.gender = gender;
+        this.phone = phone;
+        this.profileImg = profileImg;
+        this.introduce =introduce;
     }
 
     //편의 메서드
-    public void update(String username, String password) {
-        this.username=username;
+    public void update(String username, String password, String nickname, String gender, String phone, String profileImg, String introduce) {
+        this.username = username;
         this.password = password;
+        this.nickname = nickname;
+        this.gender = gender;
+        this.phone = phone;
+        this.profileImg = profileImg;
+        this.introduce =introduce;
     }
 }
