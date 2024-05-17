0c72be6ce4c90c0625428ed682fb557225980dfe
wlstj <22020306jinseoyeon@gmail.com>
2024-05-17 20:49:37 +0900

    User Entity 추가

---
+++ src/main/java/com/example/blog/user/domain/User.java
@@ -0,0 +1,80 @@
+package com.example.blog.user.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "userid")
+    private Long userid;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "profile_image")
+    private String profileImage;
+
+    @Column(name = "register_date")
+    private LocalDateTime registerDate;
+
+    @Column(name = "active")
+    private boolean active;
+
+    @Builder
+    public User(String username, String password, String email, String profileImage, LocalDateTime registerDate) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.profileImage = profileImage;
+        this.registerDate = registerDate;
+        this.active=true;
+    }
+
+    // 편의 메서드
+    //수정
+    public void updateUsername(String username) {
+        this.username = username;
+    }
+    public void updatePassword(String password) {
+        this.password=password;
+    }
+    public void updateEmail(String email) {
+        this.email = email;
+    }
+    public void updateProfileImage(String profileImage) {
+        this.profileImage = profileImage;
+    }
+    public void updateUserInfo(String username, String email, String profileImage) {
+        this.username = username;
+        this.email = email;
+        this.profileImage = profileImage;
+    }
+
+    //비밀번호 검증
+    public boolean checkPassword(String password) {
+        return this.password.equals(password);
+    }
+
+    //탈퇴
+    public void deactivateUser() {
+        this.active = false;
+    }
+    public void activateUser() {
+        this.active = true;
+    }
+}
