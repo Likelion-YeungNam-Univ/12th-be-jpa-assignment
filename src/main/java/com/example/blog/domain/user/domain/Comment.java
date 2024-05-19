d195983acf42779272e703cde96c9a06267b0871
icegosimperson <hooho931@gmail.com>
2024-05-20 00:12:51 +0900

    Add Comment entity

---
+++ src/main/java/com/example/blog/domain/user/domain/Comment.java
@@ -0,0 +1,29 @@
+package com.example.blog.domain.user.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+
+@Entity
+@NoArgsConstructor
+@Getter
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user; // User entity
+
+    @Column(nullable = false)
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id")
+    private Post post; // Post entity
+}
