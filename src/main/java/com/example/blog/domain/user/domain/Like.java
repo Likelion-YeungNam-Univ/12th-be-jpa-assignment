4e5e9e54af28a696546339a12ab7557e80eb5b7b
icegosimperson <hooho931@gmail.com>
2024-05-20 00:35:51 +0900

    Add Like entity

---
+++ src/main/java/com/example/blog/domain/user/domain/Like.java
@@ -0,0 +1,36 @@
+package com.example.blog.domain.user.domain;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Like {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "user_id", nullable = false)
+    private int userId;
+
+    public Like() {
+    }
+
+    public Like(int userId) {
+        this.userId = userId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+}
