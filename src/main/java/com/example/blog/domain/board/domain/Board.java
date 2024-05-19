04ec65f13506702fc2a6d798590597a67f2c6c2e
iampingu99 <154869950+iampingu99@users.noreply.github.com>
2024-05-19 15:51:52 +0900

    feat: Board Entity 생성

--- src/main/java/com/example/blog/domain/board/domain/Board.java
+++ src/main/java/com/example/blog/domain/board/domain/Board.java
@@ -1,2 +1,36 @@
-package com.example.blog.domain.board.domain;public class Board {
+package com.example.blog.domain.board.domain;
+
+import com.example.blog.global.util.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Board extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String title;
+    @Column(nullable = false)
+    private String content;
+    private int viewCount;
+
+    @Builder
+    public Board(String title, String content) {
+        this.title = title;
+        this.content = content;
+        this.viewCount = 0;
+    }
+
+    public void update(String title, String content){
+        this.title = title;
+        this.content = content;
+    }
+
+    public void view(){
+        viewCount++;
+    }
 }
