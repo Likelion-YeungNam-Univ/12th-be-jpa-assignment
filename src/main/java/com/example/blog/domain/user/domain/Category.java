aabd0de1c4a3f25118c5233061f20df8cfcc8eb5
ParkHa <ugauga19892556@gmail.com>
2024-05-19 23:25:02 +0900

    카테고리 엔티티 추가 및 수정

--- src/main/java/com/example/blog/domain/user/domain/Category.java
+++ src/main/java/com/example/blog/domain/user/domain/Category.java
@@ -1,12 +1,16 @@
 package com.example.blog.domain.user.domain;
 
 import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED) // Default constructor access restriction
 public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -14,4 +18,17 @@ public class Category {
 
     @Column(nullable = false, length = 255)
     private String categoryContent;
+
+    // Builder pattern
+    @Builder
+    public Category(String categoryContent) {
+        this.categoryContent = categoryContent;
+    }
+
+    // Convenience method
+    public void updateCategoryContent(String categoryContent) {
+        if (categoryContent != null) {
+            this.categoryContent = categoryContent;
+        }
+    }
 }
