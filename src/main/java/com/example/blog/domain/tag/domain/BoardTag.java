acc4e7a0af49d452a56f2e577e923c05d6adee1b
iampingu99 <154869950+iampingu99@users.noreply.github.com>
2024-05-19 15:54:15 +0900

    feat: Tag Entity 생성

--- src/main/java/com/example/blog/domain/tag/domain/BoardTag.java
+++ src/main/java/com/example/blog/domain/tag/domain/BoardTag.java
@@ -1,2 +1,16 @@
-package com.example.blog.domain.tag.domain;public class BoardTag {
+package com.example.blog.domain.tag.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class BoardTag {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 }
--- src/main/java/com/example/blog/domain/tag/domain/Tag.java
+++ src/main/java/com/example/blog/domain/tag/domain/Tag.java
@@ -1,2 +1,21 @@
-package com.example.blog.domain.tag.domain;public class Tag {
+package com.example.blog.domain.tag.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Tag {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true, nullable = false)
+    private String name;
+
+    @Builder
+    public Tag(String name) {
+        this.name = name;
+    }
 }
