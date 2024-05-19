303ceff96d1c0dddd8d14ddbe5ee83d66eff02e5
wlstj <22020306jinseoyeon@gmail.com>
2024-05-19 14:51:27 +0900

    User entity 추가

--- src/main/java/com/example/blog/domain/user/domain/User.java
+++ src/main/java/com/example/blog/domain/user/domain/User.java
@@ -6,26 +6,75 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
-public class User{
+public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    @Column(name = "userid")
+    private Long userid;
+
+    @Column(name = "username")
     private String username;
+
+    @Column(name = "password")
     private String password;
 
-    // 빌더 패턴
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "profile_image")
+    private String profileImage;
+
+    @Column(name = "register_date")
+    private LocalDateTime registerDate;
+
+    @Column(name = "active")
+    private boolean active;
+
     @Builder
-    public User(String username, String password) {
+    public User(String username, String password, String email, String profileImage, LocalDateTime registerDate) {
         this.username = username;
         this.password = password;
+        this.email = email;
+        this.profileImage = profileImage;
+        this.registerDate = registerDate;
+        this.active=true;
     }
 
-    //편의 메서드
-    public void update(String username, String password) {
-        this.username=username;
-        this.password = password;
+    // 편의 메서드
+    //수정
+    public void updateUsername(String username) {
+        this.username = username;
+    }
+    public void updatePassword(String password) {
+        this.password=password;
+    }
+    public void updateEmail(String email) {
+        this.email = email;
+    }
+    public void updateProfileImage(String profileImage) {
+        this.profileImage = profileImage;
+    }
+    public void updateUserInfo(String username, String email, String profileImage) {
+        this.username = username;
+        this.email = email;
+        this.profileImage = profileImage;
+    }
+
+    //비밀번호 검증
+    public boolean checkPassword(String password) {
+        return this.password.equals(password);
+    }
+
+    //탈퇴
+    public void deactivateUser() {
+        this.active = false;
+    }
+    public void activateUser() {
+        this.active = true;
     }
 }
