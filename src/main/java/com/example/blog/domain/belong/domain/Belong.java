cc80378f0fb7e01f1d585196cbef8d3dcc254d19
tentenball <lth8905@gmail.com>
2024-05-19 12:57:32 +0900

    Feat: belong domain implemented

---
+++ src/main/java/com/example/blog/domain/belong/domain/Belong.java
@@ -0,0 +1,16 @@
+package com.example.blog.domain.belong.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class Belong {
+    @EmbeddedId
+    private BelongId Id;
+}
