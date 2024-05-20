b5c962e84cd6fd587d8abbcb667090a9b6065217
asn6878 <asn6878@gmail.com>
2024-05-21 04:53:16 +0900

    @Repository 어노테이션 누락 수정

--- src/main/java/com/example/blog/domain/post/repository/PostRepository.java
+++ src/main/java/com/example/blog/domain/post/repository/PostRepository.java
@@ -2,6 +2,8 @@ package com.example.blog.domain.post.repository;
 
 import com.example.blog.domain.post.domain.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
 }
