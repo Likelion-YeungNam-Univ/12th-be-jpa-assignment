ad3349e55833b669463676ae5d8b7ba1b4aabdff
asn6878 <asn6878@gmail.com>
2024-05-21 05:23:39 +0900

    네이밍 규칙 메서드 작성

--- src/main/java/com/example/blog/domain/post/repository/PostRepository.java
+++ src/main/java/com/example/blog/domain/post/repository/PostRepository.java
@@ -4,6 +4,12 @@ import com.example.blog.domain.post.domain.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
+
+    Optional<Post> findByTitle(String title);
+
+    Optional<Post> findByTitleOrContent(String title, String content);
 }
