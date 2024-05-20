8d183f5100255b06aa2220279c301d18b4cda11f
Hyewon <hooho931@gmail.com>
2024-05-20 17:45:28 +0900

    Revert "Entity 생성 - 진서연"

--- src/main/java/com/example/blog/domain/user/domain/User.java
+++ src/main/java/com/example/blog/domain/user/domain/User.java
@@ -5,84 +5,27 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.antlr.v4.runtime.misc.NotNull;
-import org.hibernate.annotations.ColumnDefault;
-
-import java.time.LocalDateTime;
 
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
-public class User {
+public class User{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "user_id")
-    private Long userid;
-
-    @Column(name = "user_name")
-    @NotNull
+    private Long id;
     private String username;
-
-    @Column(name = "password")
-    @NotNull
     private String password;
 
-    @Column(name = "email")
-    @NotNull
-    private String email;
-
-    @Column(name = "profile_image")
-    @NotNull
-    private String profileImage;
-
-    @Column(name = "register_date")
-    @NotNull
-    private LocalDateTime registerDate;
-
-    @Column(name = "active")
-    @NotNull
-    @ColumnDefault("true")
-    private boolean active;
-
+    // 빌더 패턴
     @Builder
-    public User(String username, String password, String email, String profileImage, LocalDateTime registerDate) {
+    public User(String username, String password) {
         this.username = username;
         this.password = password;
-        this.email = email;
-        this.profileImage = profileImage;
-        this.registerDate = registerDate;
     }
 
-    // 편의 메서드
-    //수정
-    public void updateUsername(String username) {
-        this.username = username;
-    }
-    public void updatePassword(String password) {
-        this.password=password;
-    }
-    public void updateEmail(String email) {
-        this.email = email;
-    }
-    public void updateProfileImage(String profileImage) {
-        this.profileImage = profileImage;
-    }
-    public void updateUserInfo(String username, String email, String profileImage) {
-        this.username = username;
-        this.email = email;
-        this.profileImage = profileImage;
-    }
-
-    //비밀번호 검증
-    public boolean checkPassword(String password) {
-        return this.password.equals(password);
-    }
-
-    //탈퇴
-    public void deactivateUser() {
-        this.active = false;
-    }
-    public void activateUser() {
-        this.active = true;
+    //편의 메서드
+    public void update(String username, String password) {
+        this.username=username;
+        this.password = password;
     }
 }
