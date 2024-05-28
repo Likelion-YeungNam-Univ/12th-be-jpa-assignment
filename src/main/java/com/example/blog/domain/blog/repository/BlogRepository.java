79c5a1efdf0341a6c3c37c1d971d73ab73cb2313
Jsplix <jkjin112@gmail.com>
2024-05-29 00:42:10 +0900

    Blog 관련 repository & service 메소드 수정 및 추가

--- src/main/java/com/example/blog/domain/blog/repository/BlogRepository.java
+++ src/main/java/com/example/blog/domain/blog/repository/BlogRepository.java
@@ -3,5 +3,8 @@ package com.example.blog.domain.blog.repository;
 import com.example.blog.domain.blog.domain.Blog;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface BlogRepository extends JpaRepository<Blog, Long> {
+    Optional<Blog> findByName(String name);
 }
--- src/main/java/com/example/blog/domain/blog/service/BlogService.java
+++ src/main/java/com/example/blog/domain/blog/service/BlogService.java
@@ -1,4 +1,23 @@
 package com.example.blog.domain.blog.service;
 
+import com.example.blog.domain.blog.domain.Blog;
+import com.example.blog.domain.blog.repository.BlogRepository;
+import com.example.blog.domain.user.domain.User;
+import com.example.blog.domain.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class BlogService {
+
+    private final BlogRepository blogRepository;
+    private final UserRepository userRepository;
+
+    public void createBlog(Blog blog) {
+        User user = userRepository.findByNickname(blog.getName())
+                .orElseThrow(() -> new IllegalStateException("user is not found."));
+        blog.setUser(user);
+        blogRepository.save(blog);
+    }
 }
