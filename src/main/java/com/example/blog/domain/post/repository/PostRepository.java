c5adf7a73faceb5de9e83021874c6fb707acb3ec
DaeYoung Park <pdy000726@naver.com>
2024-05-29 01:18:06 +0900

    feat - 조회수 증가 쿼리 + 게시물 검색 네이밍 추가

--- src/main/java/com/example/blog/domain/post/repository/PostRepository.java
+++ src/main/java/com/example/blog/domain/post/repository/PostRepository.java
@@ -1,9 +1,14 @@
 package com.example.blog.domain.post.repository;
 
 import com.example.blog.domain.post.domain.Post;
+import com.example.blog.domain.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -12,4 +17,12 @@ public interface PostRepository extends JpaRepository<Post, Long> {
     Optional<Post> findByTitle(String title);
 
     Optional<Post> findByTitleOrContent(String title, String content);
+
+    List<Post> findByTitleContaining(String title);
+    List<Post> findByContentContaining(String content);
+    List<Post> findByUser(User user);
+
+    @Modifying
+    @Query("update Post p set p.view = p.view + 1 where p.id = :id")
+    void updateView(@Param("id") Long id);
 }
