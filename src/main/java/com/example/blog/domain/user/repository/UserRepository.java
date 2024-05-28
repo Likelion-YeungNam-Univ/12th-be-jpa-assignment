bfadcf8a000eb2e9e2f8d41a371a45efd0c0aadd
Jsplix <jkjin112@gmail.com>
2024-05-28 13:43:24 +0900

    Spring JPA 사용을 위한 User Repository 생성

---
+++ src/main/java/com/example/blog/domain/user/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package com.example.blog.domain.user.repository;
+
+import com.example.blog.domain.user.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+    Optional<User> findByNickname(String nickname);
+
+}
