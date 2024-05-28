305a604a355e48179aadf5bd36f522a58082be7d
DaeYoung Park <pdy000726@naver.com>
2024-05-29 01:49:08 +0900

    feat - comment dto 구현

--- src/main/java/com/example/blog/domain/comment/dto/CommentReq.java
+++ src/main/java/com/example/blog/domain/comment/dto/CommentReq.java
@@ -1,2 +1,17 @@
-package com.example.blog.domain.comment.dto;public class CommentReq {
+package com.example.blog.domain.comment.dto;
+
+import com.example.blog.domain.comment.domain.Comment;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class CommentReq {
+
+    private String content;
+
+    public Comment toCommentEntity() {
+        return Comment.builder()
+                .content(content)
+                .build();
+    }
 }
--- src/main/java/com/example/blog/domain/comment/dto/CommentRes.java
+++ src/main/java/com/example/blog/domain/comment/dto/CommentRes.java
@@ -1,2 +1,20 @@
-package com.example.blog.domain.comment.dto;public class CommentRes {
+package com.example.blog.domain.comment.dto;
+
+import com.example.blog.domain.comment.domain.Comment;
+import lombok.Getter;
+
+@Getter
+public class CommentRes {
+
+    private final Long id;
+    private final String content;
+    private final String postTitle;
+    private final String username;
+
+    public CommentRes(Comment comment) {
+        this.id = comment.getId();
+        this.content = comment.getContent();
+        this.postTitle = comment.getPost().getTitle();
+        this.username = comment.getUser().getUsername();
+    }
 }
