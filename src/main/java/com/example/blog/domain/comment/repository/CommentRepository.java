ffbc69cebb05841220e0eadc89bf33fa2131455b
DaeYoung Park <pdy000726@naver.com>
2024-05-29 01:49:28 +0900

    feat - comment service 구현

--- src/main/java/com/example/blog/domain/comment/repository/CommentRepository.java
+++ src/main/java/com/example/blog/domain/comment/repository/CommentRepository.java
@@ -1,2 +1,13 @@
-package com.example.blog.domain.comment.repository;public interface CommentRepository {
+package com.example.blog.domain.comment.repository;
+
+import com.example.blog.domain.comment.domain.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+    List<Comment> findByPost_Id(Long postId);
 }
--- src/main/java/com/example/blog/domain/comment/service/CommentService.java
+++ src/main/java/com/example/blog/domain/comment/service/CommentService.java
@@ -1,2 +1,73 @@
-package com.example.blog.domain.comment.service;public class CommentService {
+package com.example.blog.domain.comment.service;
+
+import com.example.blog.domain.comment.domain.Comment;
+import com.example.blog.domain.comment.dto.CommentReq;
+import com.example.blog.domain.comment.dto.CommentRes;
+import com.example.blog.domain.comment.repository.CommentRepository;
+import com.example.blog.domain.post.domain.Post;
+import com.example.blog.domain.post.repository.PostRepository;
+import com.example.blog.domain.user.domain.User;
+import com.example.blog.domain.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
+@Service
+public class CommentService {
+
+    private final CommentRepository commentRepository;
+    private final PostRepository postRepository;
+    private final UserRepository userRepository;
+
+    /* 댓글 생성. */
+    @Transactional
+    public CommentRes save(Long userId, Long postId, CommentReq commentReq) {
+
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new IllegalArgumentException("해당 유저 없음!"));
+        Post post = postRepository.findById(postId)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글 없음!"));
+
+
+        Comment comment = commentReq.toCommentEntity();
+        comment.setUser(user);
+        comment.setPost(post);
+
+        return new CommentRes(commentRepository.save(comment));
+    }
+
+    /* 게시글 전체 댓글 찾기. */
+    @Transactional(readOnly = true)
+    public List<CommentRes> findAll(Long postId) {
+
+        List<Comment> comments = commentRepository.findByPost_Id(postId);
+        return comments.stream().map(CommentRes::new).collect(Collectors.toList());
+    }
+
+
+    /* 댓글 업데이트. */
+    @Transactional
+    public CommentRes update(Long commentId, CommentReq commentReq) {  // update
+
+        Comment comment = commentRepository.findById(commentId)
+                .orElseThrow(() -> new IllegalArgumentException("해당 댓글 없음!"));
+
+        comment.updateContent(commentReq.getContent());
+
+        return new CommentRes(comment);
+    }
+
+    /* 댓글 삭제. */
+    @Transactional
+    public void delete(Long commentId) {
+
+        Comment comment = commentRepository.findById(commentId)
+                .orElseThrow(() -> new IllegalArgumentException("해당 댓글 없음!"));
+
+        commentRepository.delete(comment);
+    }
 }
