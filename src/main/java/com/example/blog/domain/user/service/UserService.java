0d85a2ced1060d38ab5caa391db92011062a3190
icegosimperson <hooho931@gmail.com>
2024-05-28 14:52:46 +0900

    사용자 CRUD

--- src/main/java/com/example/blog/domain/user/service/UserService.java
+++ src/main/java/com/example/blog/domain/user/service/UserService.java
@@ -1,25 +1,49 @@
 package com.example.blog.domain.user.service;
 
+import com.example.blog.domain.user.dto.UserReq;
+import com.example.blog.domain.user.dto.UserRes;
 import com.example.blog.domain.user.domain.User;
 import com.example.blog.domain.user.repository.UserRepository;
-import lombok.RequiredArgsConstructor;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
-@RequiredArgsConstructor
+@AllArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
 
-    // Create
-    public User createUser(String username, String email, String password){
-        User user = User.builder()
-                .username(username)
-                .email(email)
-                .password(password)
-                .build();
+    public List<UserRes> getAllUsers() {
+        List<User> users = userRepository.findAll();
+        return users.stream()
+                .map(UserRes::fromEntity)
+                .collect(Collectors.toList());
+    }
+
+    public User getUser(Long userId) {
+        return userRepository.findById(userId)
+                .orElseThrow(() -> new IllegalArgumentException("해당 유저 없음"));
+    }
+
+    public User createUser(UserReq request) {
+        User user = request.toEntity();
+
         return userRepository.save(user);
     }
 
+    public User updateUser(Long userId, UserReq request) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new IllegalArgumentException("해당 유저 없음"));
 
+        // 여기는 save 안해주나요??
+        user.update(request.username(), request.password());
+
+        return user;
+    }
+
+    public void deleteUser(Long userId) {
+        userRepository.deleteById(userId);
+    }
 }
