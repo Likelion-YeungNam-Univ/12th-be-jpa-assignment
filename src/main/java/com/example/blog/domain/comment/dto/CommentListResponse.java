02ba86d98e8374fa58fe31d211aed876c019deca
iampingu99 <154869950+iampingu99@users.noreply.github.com>
2024-05-29 23:58:10 +0900

    feat: post read reponse comment 추가

--- src/main/java/com/example/blog/domain/comment/dto/CommentListResponse.java
+++ src/main/java/com/example/blog/domain/comment/dto/CommentListResponse.java
@@ -1,2 +1,17 @@
-package com.example.blog.domain.comment.dto;public class CommentListResponse {
+package com.example.blog.domain.comment.dto;
+
+import com.example.blog.domain.comment.doamin.Comment;
+
+public record CommentListResponse(
+        Long userId,
+        String content,
+        int likeCount
+) {
+    public static CommentListResponse fromEntity(Comment comment){
+        return new CommentListResponse(
+                comment.getUser().getId(),
+                comment.getContent(),
+                comment.getLikeCount()
+        );
+    }
 }
--- src/main/java/com/example/blog/domain/post/dto/PostReadResponseDto.java
+++ src/main/java/com/example/blog/domain/post/dto/PostReadResponseDto.java
@@ -1,19 +1,24 @@
 package com.example.blog.domain.post.dto;
 
+import com.example.blog.domain.comment.dto.CommentListResponse;
 import com.example.blog.domain.post.domain.Post;
+
+import java.util.List;
 
 public record PostReadResponseDto(
         String username,
         String title,
         String content,
-        int viewCount
+        int viewCount,
+        List<CommentListResponse> comments
 ) {
     public static PostReadResponseDto fromEntity(Post post){
         return new PostReadResponseDto(
                 post.getUser().getUsername(),
                 post.getTitle(),
                 post.getContent(),
-                post.getViewCount()
+                post.getViewCount(),
+                post.getComments().stream().map(CommentListResponse::fromEntity).toList()
         );
     }
 }
