d2f6faa6ee35a3cb64bfcab6f3700b9f53de1f11
Jsplix <jkjin112@gmail.com>
2024-05-30 08:40:24 +0900

    Column 어노테이션 추가

--- src/main/java/com/example/blog/util/BaseTimeEntity.java
+++ src/main/java/com/example/blog/util/BaseTimeEntity.java
@@ -1,5 +1,6 @@
 package com.example.blog.util;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
@@ -13,9 +14,12 @@ import java.time.LocalDateTime;
 @EntityListeners(AuditingEntityListener.class)
 @Getter
 public class BaseTimeEntity {
+
     @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
     private LocalDateTime createdAt;
 
     @LastModifiedDate
+    @Column(name = "updated_at", nullable = false)
     private LocalDateTime updatedAt;
 }
