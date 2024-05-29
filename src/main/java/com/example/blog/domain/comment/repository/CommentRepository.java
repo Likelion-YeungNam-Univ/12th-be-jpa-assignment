0c62845f75e317e73180ed5f234bfe580eb5c561
Jsplix <jkjin112@gmail.com>
2024-05-29 14:18:29 +0900

    CommentRepository 생성

---
+++ src/main/java/com/example/blog/domain/comment/repository/CommentRepository.java
@@ -0,0 +1,7 @@
+package com.example.blog.domain.comment.repository;
+
+import com.example.blog.domain.comment.domain.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+}
